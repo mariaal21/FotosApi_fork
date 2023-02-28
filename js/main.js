1c3bcbc41de8d7ac3b100b0bff353fa76df37aa5
Barneto <mdpa23@gmail.com>
2023-02-28 11:39:12 +0100

    prueba

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-alert("hola")
+document.addEventListener('DOMContentLoaded',()=>{
+
+    //* VARIABLES
+
+
+
+
+
+    //* EVENTOS      
+
+
+    //* FUNCIONES  
+
+
+    const init=()=>{
+
+        const url=location.search
+        console.log(url);
+
+        let params = new URLSearchParams(url);
+        
+        console.log(params);
+
+
+}
+ init()
+
+
+
+
+
+})//LOAD
